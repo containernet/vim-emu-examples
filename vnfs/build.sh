23c7b828701a776bb87bbcdb1d629caa6dd67604
peusterm <manuel.peuster@uni-paderborn.de>
2018-12-04 16:09:15 +0100

    Initial repo structure
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- vnfs/build.sh
+++ vnfs/build.sh
@@ -4,4 +4,4 @@ set -e
 target_repo=${1-mpeuster}
 
 docker build -t $target_repo/vimemu-vnf-fw-ovs -f vimemu-vnf-fw-ovs/Dockerfile vimemu-vnf-fw-ovs
-docker build -t $target_repovimemu-vnf-ids-snort -f vimemu-vnf-ids-snort/Dockerfile vimemu-vnf-ids-snort
+docker build -t $target_repo/vimemu-vnf-ids-snort -f vimemu-vnf-ids-snort/Dockerfile vimemu-vnf-ids-snort
